c80dbe4b5f0cd8515910703162541659aaccc515
Gabriel P??rez <gabriel@garox.org>
2022-02-22 09:44:39 -0300

    add movement

---
+++ Assets/HealthManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HealthManager : MonoBehaviour
+{
+    public int initialHealth = 100;
+    public int maxHealth = 100;
+    private int currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = initialHealth;
+
+    }
+    void Update()
+    {
+        if (currentHealth <= 0)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+
+    public int getCurrentHealth()
+    {
+        return currentHealth;
+    }
+
+    public float getCurrentHealhPercentage()
+    {
+
+        return (float)currentHealth / (float)maxHealth * 100f;
+    }
+    public void onReceiveAttack(int attack)
+    {
+        currentHealth -= attack;
+        currentHealth = Mathf.Max(0, currentHealth);
+    }
+    public void onReceiveHeal(int heal)
+    {
+        currentHealth += heal;
+        currentHealth = Mathf.Min(maxHealth, currentHealth);
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 10f;
+    public float mouseSensitivity = 100f;
+    public PointsManager pointsManager;
+    public HealthManager healthManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        applyMovement();
+        mouseLook();
+    }
+
+    void applyMovement()
+    {
+        float horizontalDelta = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float verticalDelta = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        transform.Translate(horizontalDelta, 0, verticalDelta);
+
+    }
+
+    void mouseLook()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        // float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        transform.Rotate(Vector3.up * mouseX);
+    }
+    private void OnCollisionEnter(Collision collison)
+    {
+        Debug.Log(collison.gameObject.tag + " collision");
+
+    }
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Point")
+        {
+            Destroy(collider.gameObject);
+            pointsManager.onGetCoin();
+            healthManager.onReceiveAttack(10);
+        }
+        Debug.Log(collider.gameObject.tag);
+
+    }
+}
